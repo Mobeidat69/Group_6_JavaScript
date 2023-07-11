c3f94cb3c2714d1fba8a3a48b4e2eb5faa464f65
laithy0 <laithyaseen955@gmail.com>
2023-07-11 13:28:48 +0300

    functionE

---
+++ functionE/function.js
@@ -0,0 +1,21 @@
+let x = function(x,z,y){
+
+    if(z == "+"){
+        console.log(x + y);
+    }
+
+    if(z == "-"){
+        console.log(x - y);
+    }
+
+    if(z == "/"){
+        console.log(x / y);
+    }
+
+    if(z == "*"){
+        console.log(x * y);
+    }
+
+}
+
+x(5, "*" , 5)
