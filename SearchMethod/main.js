a23386e8f41190f3c3aae9e18d4ed68198043b4c
AnasWebDev <anasyasien101@gmail.com>
2023-07-12 12:42:50 +0300

    update

--- SearchMethod/main.js
+++ SearchMethod/main.js
@@ -2,9 +2,14 @@
 var text = 'The rain in SPAIN stays mainly in the plain'
 
 var position1 = text.search('ain') // result: 5
-var position2 = text.search(/ain/) // result: 5
-var position3 = text.search(/AIN/) // result: 14
-var position4 = text.search(/AIN/i) //result: 5
-var position5 = text.search(/w/) // result: -1
+ position2 = text.search(/ain/) // result: 5
+ position3 = text.search(/AIN/) // result: 14
+ position4 = text.search(/AIN/i) //result: 5
+ position5 = text.search(/w/) // result: -1
 
+
+console.log(position1);
+console.log(position2);
+console.log(position3);
+console.log(position4);
 console.log(position5);
